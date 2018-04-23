a4469469d57e1ff36a039c5094658528080e10c8
Amber Sharpe <amberjsharpe@gmail.com>
2018-04-22 19:31:31 -0500

    added nav bar and social media bar to home page

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './Nav';
+import Introduction from './Introduction';
+import Social from './Social';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
+          <Nav />
+          <Social />
+          <Introduction />
         </header>
       </div>
     );
